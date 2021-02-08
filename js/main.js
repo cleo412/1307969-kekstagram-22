97e17a409d6d81a0a02238856b3ce7a13e2f1c65
cleo412 <semenenko412@gmail.com>
2021-02-08 13:51:39 +0300

    2 задание

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict'
+/* ссылкa на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+let ERROR_IN_PARAMETERS = 'Ошибка';
+let getRandNum = function (min, max) {
+  if (min < 0 || max < 0 || max <= min) {
+    return ERROR_IN_PARAMETERS;
+  }
+  return Math.floor(Math.random() * ((max - min) + min));
+}
+
+/* ссылкa на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length*/
+
+let getMaxlength = function (str, controlLenghtStr) {
+  return str.lenght > controlLenghtStr;
+}
