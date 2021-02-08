322a63d57398a1bf108957502eea11f7ed46b10a
cleo412 <semenenko412@gmail.com>
2021-02-08 17:47:10 +0300

    дом. задание #2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 'use strict'
-/* ссылкa  на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
+/*https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random*/
 let ERROR_IN_PARAMETERS = 'Ошибка';
 let getRandNum = function (min, max) {
   if (min < 0 || max < 0 || max <= min) {
     return ERROR_IN_PARAMETERS;
   } return Math.floor(Math.random() * ((max - min) + min));
 }
-/* ссылкa на источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length*/
+
+getRandNum(10, 20)
+
+/*https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length*/
 let getMaxlength = function (str, controlLenghtStr) {
   return str.lenght > controlLenghtStr;
 }
+
+getMaxlength(10, 20)
